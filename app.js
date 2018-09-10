90dbe7ee12a85bfa446c63f581f61bc78b6bd51f
Leah <ljoberh@gmail.com>
2018-09-10 13:40:44 -0600

    assignment 6 completed

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 function onReady() {
   const toDos = [];
   const addToDoForm = document.getElementById('addToDoForm');
+  let id = 0;
 
   function createNewToDo() {
     const newToDoText = document.getElementById('newToDoText');
     if (!newToDoText.value) { return; }
 
+
     toDos.push({
       title: newToDoText.value,
-      complete: false
+      complete: false,
+      id: ++id
     });
     newToDoText.value = '';
 
@@ -23,14 +26,36 @@ function onReady() {
     toDos.forEach(function(toDo) {
       const newLi = document.createElement('li');
       const checkbox = document.createElement('input');
+      const deleteButton = document.createElement('button');
       checkbox.type = "checkbox";
+      deleteButton.type = "button";
+      deleteButton.textContent = "Delete";
 
       newLi.textContent = toDo.title;
 
       toDoList.appendChild(newLi);
       newLi.appendChild(checkbox);
-    });
+      newLi.appendChild(deleteButton);
 
+      deleteButton.addEventListener ("click", event =>{
+        event.preventDefault()
+
+        function removeItem(){
+          for (var i = 0; i <toDos.length; i++){
+            if(toDo.id < i && id == i){
+              return true;
+            }else {
+              event.target.parentElement.remove()
+            }
+          }
+        }
+        return toDos.filter(removeItem);
+        renderTheUI()
+      });
+
+
+
+    });
   }
 
   addToDoForm.addEventListener('submit', event => {
